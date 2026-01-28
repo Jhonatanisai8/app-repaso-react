0de15eb54ff4196b103560feb02ccff7810fda80
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-01-27 22:32:07 -0500

    Implementa la funcionalidad para agregar cursos en el componente CourseList usando useState y un botón de acción.

--- src/components/maps/ListaCursos.jsx
+++ src/components/maps/ListaCursos.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Item = ({nombre, lenguaje}) => {
+
+const Item = ({ nombre, lenguaje }) => {
   return (
     <li>
       {nombre} - <strong>{lenguaje}</strong>
@@ -9,22 +10,28 @@ const Item = ({nombre, lenguaje}) => {
 }
 
 const ListaCursos = () => {
+
   const cursos = [
     { id: 1, nombre: "Java Developer", lenguaje: "Java" },
     { id: 2, nombre: "React para principiantes", lenguaje: "JavaScript" },
     { id: 3, nombre: "Spring boot Pro", lenguaje: "Java" },
   ]
+  const [arreglo, setArreglo] = useState(cursos)
+  const agregarTarea = () => {
+    const nuevoCurso = { id: Date.now(), nombre: "Python Developer", lenguaje: "Python" }
+    setArreglo([...arreglo, nuevoCurso])
+  }
+
   return (
     <div>
       <ul>
         {
-          cursos.map((curso) => (
+          arreglo.map((curso) => (
             <Item key={curso.id} nombre={curso.nombre} lenguaje={curso.lenguaje}></Item>
           ))
         }
       </ul>
-
-
+      <button onClick={() => agregarTarea()}>Agregar Curso</button>
     </div>
   )
 }
